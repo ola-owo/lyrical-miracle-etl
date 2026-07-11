438edce855851bc25290e63351f695bdc498a8bd
Ola <ola.owoputi@gmail.com>
2026-07-11 19:09:59 -0400

    add shortcut script to start docker container

---
+++ docker-run.sh
@@ -0,0 +1,6 @@
+# Start the docker container
+exec docker run --rm -it -p 8080:8080 --env-file .env \
+  --mount type=volume,src=airflow-logs,dst=/opt/airflow/logs \
+  --mount type=bind,src=$(pwd)/data,dst=/data,ro \
+  --mount type=bind,src=$(pwd)/keys,dst=/opt/airflow/keys,ro \
+  lyrical-miracle-etl standalone
